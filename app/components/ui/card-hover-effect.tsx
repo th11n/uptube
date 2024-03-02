b939c78e4dfac3949b1da4a16e611814558812a6
th11n <thinex@wp.pl>
2024-03-02 17:50:30 +0100

    refactor: quick changes

--- app/components/ui/card-hover-effect.tsx
+++ app/components/ui/card-hover-effect.tsx
@@ -23,6 +23,7 @@ export const HoverEffect = ({
     >
       {items.map((item, idx) => (
         <a
+          key={item?.title}
           className="relative group  block p-2 h-full w-full"
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
